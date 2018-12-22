48dd493ff39c21639a49a2091edd7eaa6da01e34
okateim <hoge@example.com>
2018-12-22 19:34:56 +0900

    change

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,11 +23,11 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/mietaku'),
+          )
 
 DEFAULT_PAGINATION = 10
 
